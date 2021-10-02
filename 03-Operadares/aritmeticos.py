8d262fd0e0c61162ee4f0ec2c9b594532b13d899
jdrosas <juandiazjdrd@gmail.com>
2021-10-02 12:19:10 -0500

    Operadores

---
+++ 03-Operadares/aritmeticos.py
@@ -0,0 +1,21 @@
+#Operdores aritmeticos
+
+#Operador asignacion \" = \"
+numero1 = 80
+numero2 = 4
+
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+resto = numero1 % numero2
+
+print("**************** CALCULADORA *********************")
+print(f"La suma es: {numero1 + numero2}")
+print(f"La resta es: {resta}")
+print(f"La mutiplicaciones es: {multiplicacion}")
+print(f"La division es: {division}")
+print(f"El restante de la division es: {resto}")
+
+
+
+
---
+++ 03-Operadares/asignacion.py
@@ -0,0 +1,33 @@
+# Operadores de asigncion
+edad = 20
+print(edad)
+
+edad += 5
+#edad = edad + 5
+edad -= 15
+print(edad)
+
+# Operadores incremento y decremento
+year = 2021
+
+# Incremento
+#year = year + 1
+year += 1
+
+# Decremento 
+#year = year - 1
+year -= 5
+
+# Pre incremento
+year = 1 + year
+
+# Pre decremento
+year = 1 + year
+
+print(year)
+"""
+year++
+year--
+--year
+++year"""
+
