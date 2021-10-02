3dcd60e4379ef292893fc608a23e28408f1768e7
jdrosas <juandiazjdrd@gmail.com>
2021-10-02 12:32:54 -0500

    Entradas y salidas

---
+++ 04-Entrada-salida/entrada-salida.py
@@ -0,0 +1,7 @@
+# Entrada
+nombre = input("¿Cual es tu nombre?: ")
+ciudad = input("¿De que ciudad eres?: ")
+
+# Salida
+print(f"Bienvenido, en que puedo ayudarte {nombre}?, veo que eres de la ciudad de {ciudad}")
+
