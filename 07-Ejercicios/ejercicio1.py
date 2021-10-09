33fa842d263e187976de765550001074f668a4e6
jdrosas <juandiazjdrd@gmail.com>
2021-10-08 22:40:51 -0500

    Ejercicios

---
+++ 07-Ejercicios/ejercicio1.py
@@ -0,0 +1,23 @@
+"""
+Ejercicio 1.
+    - Crear variables una "pais" y otra "continente"
+    - Mostrar valor por pantalla
+    - Poner un comentario diciendo el tipo de dato
+"""
+
+# STR
+pais = "Colombia"
+print(pais)
+print(type(pais))
+
+# STR
+continente = "America"
+print(continente)
+print(type(continente)) 
+
+print("-----------------------------------")
+
+pais = "Colombia"           #String
+continente = "America"      #String
+
+print(f"{pais} - {continente}")
