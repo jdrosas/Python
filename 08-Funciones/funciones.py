759d26d9d3b1e5f8c5962a32f77dc7c06e1f6959
jdrosas <juandiazjdrd@gmail.com>
2021-10-23 21:50:34 -0500

    Funciones

--- 08-Funciones/funciones.py
+++ 08-Funciones/funciones.py
@@ -131,5 +131,23 @@ def calculadora(numero1, numero2, basicas = False):
 
 print(calculadora(5, 5))
 
+# Ejemplo 7
+print("-----------------------------")
+print("######### EJEMPLO 7 #########")
+print("-----------------------------")
+
+def getNombre(nombre):
+    texto = f"El nombre es: {nombre}"
+    return texto
+
+def getApellido(apellidos):
+    texto = f"Los apellidos son: {apellidos}"
+    return texto
+
+def devuelveTodo(nombre, apellidos):
+    texto = getNombre(nombre) + "\n" + getApellido(apellidos)
+    return texto
+
+print(devuelveTodo("Juan David", "Rosas Diaz"))
 
 
