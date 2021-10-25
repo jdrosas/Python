e842cdbd57d4facdf62e7a7b1bb574713b9b5713
jdrosas <juandiazjdrd@gmail.com>
2021-10-24 20:32:37 -0500

    Listas

--- 09-Listas/listas.py
+++ 09-Listas/listas.py
@@ -13,8 +13,19 @@ cantantes = list(("2pac", "Drake", "Post Malone"))
 años = list(range(2020, 2050))
 variada = ["Juan David", 30, 10.5, True, "Rosas Diaz"]
 
-print(pelicula)
+"""print(pelicula)
 print(peliculas)
 print(cantantes)
 print(años)
-print(variada)
+print(variada)"""
+
+# Indices
+pelicula = "otra cosa"
+peliculas[1] = "Pulp Fiction"
+peliculas[2] = "Reservoir Dogs"
+print(peliculas)
+
+print(peliculas[1])
+print(peliculas[-2])
+print(cantantes[1:3])
+print(peliculas[1:])
