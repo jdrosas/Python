141f2b4add554c8e941b3514335c277204a703a1
jdrosas <juandiazjdrd@gmail.com>
2021-10-24 20:23:35 -0500

    Listas

---
+++ 09-Listas/listas.py
@@ -0,0 +1,20 @@
+"""
+LISTAS (arrays)
+Son colecciones o conjuntos de datos/valores, bajo un unico
+nombre.
+Para acceder a esos valores podemos usar un indice numerico.
+"""
+
+pelicula = "Batman"
+
+# Definir lista
+peliculas = ["Batman", "Spiderman", "El señor de los anillos"]
+cantantes = list(("2pac", "Drake", "Post Malone"))
+años = list(range(2020, 2050))
+variada = ["Juan David", 30, 10.5, True, "Rosas Diaz"]
+
+print(pelicula)
+print(peliculas)
+print(cantantes)
+print(años)
+print(variada)
