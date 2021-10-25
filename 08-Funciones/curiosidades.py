b4c1e2e1312acfaa599803ad705cfd2f19b6171c
jdrosas <juandiazjdrd@gmail.com>
2021-10-24 20:08:47 -0500

    Funciones - curiosidades

---
+++ 08-Funciones/curiosidades.py
@@ -0,0 +1,16 @@
+"""Definir las funciones al prinicipio del codigo, utilizar
+return en vez de print dentro de la funcion"""
+
+def mi_funcion(nombre):
+    return "Hola que tal " + nombre
+
+def mi_funcion2(apellidos):
+    return "Hola que tal 2 " + apellidos
+
+"""Definir las variables al prinicipio del codigo"""
+
+nombre = "Juan David"
+apellidos = "Rosas Diaz"
+
+print(mi_funcion(nombre))
+print(mi_funcion2(apellidos))
