bf1d2f6a0e5781f716905ba69f398cc0bf965fba
jdrosas <juandiazjdrd@gmail.com>
2021-10-30 23:26:28 -0500

    Sets - Diccionarios

---
+++ 10-Sets-Diccionarios/diccionarios.py
@@ -0,0 +1,14 @@
+"""
+Diccionario:
+Un tipo de dato que almacena un conjunto de datos.
+En un formato clave > valor.
+Es parecido a un array asociativo o un objeto json.
+"""
+
+persona = {
+    "nombre": "Juan David",
+    "apellidos": "Rosas",
+    "web": "jrosas.com"
+}
+
+print(persona["apellidos"])
