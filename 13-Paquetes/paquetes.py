6f32e4890147870ec4263f18e9a5e26e9251f822
jdrosas <juandiazjdrd@gmail.com>
2021-11-07 10:50:41 -0500

    Paquetes

---
+++ 13-Paquetes/paquetes.py
@@ -0,0 +1,11 @@
+"""
+Paquete: conjunto de modulos
+"""
+
+print("Probando paquetes: ")
+
+from paquete_modulos import pruebas
+from paquete_modulos import herramientas
+
+pruebas.probando()
+herramientas.nombreCompleto("Juan David", "Rosas")
