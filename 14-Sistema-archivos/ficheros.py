33c671e7319180f0d0ad00716ef2796b7b32caea
jdrosas <juandiazjdrd@gmail.com>
2021-11-07 11:15:12 -0500

    Ficheros

--- 14-Sistema-archivos/ficheros.py
+++ 14-Sistema-archivos/ficheros.py
@@ -10,3 +10,23 @@ archivo.write("*********SOY UN TEXTO DESDE PYTHON********\n")
 
 # Cerar
 archivo.close()
+
+# Abrir archivo
+ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+archivo_lectura = open(ruta, "r")
+
+# Leer contenido
+# contenido = archivo_lectura.read()
+# print(contenido)
+# for elemento in contenido:
+#     print(elemento)
+
+# Leer contenido y guardarlo en lista
+lista = archivo_lectura.readlines()
+archivo_lectura.close
+
+for frase in lista:
+
+    # lista_frase = frase.split()
+    # print(lista_frase)
+    print("- "+frase.center(100))
