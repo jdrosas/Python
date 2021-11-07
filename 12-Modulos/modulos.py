84b73dc32f22c216748e2881283a625920e0aff8
jdrosas <juandiazjdrd@gmail.com>
2021-11-06 22:31:50 -0500

    Modulos

--- 12-Modulos/modulos.py
+++ 12-Modulos/modulos.py
@@ -17,3 +17,20 @@ from mimodulo import *
 
 print(holaMundo("Juan David"))
 print(calculadora(5, 4, True))
+
+# Modulo fechas
+import datetime
+
+print(datetime.date.today())
+
+fecha_completa = datetime.datetime.now()
+
+print(fecha_completa)
+print(fecha_completa.year)
+print(fecha_completa.month)
+print(fecha_completa.day)
+
+fecha_personalizada = fecha_completa.strftime("%d/%m/%Y, %H:%M:%S")
+print("Mi fecha personalizada", fecha_personalizada)
+
+print(datetime.datetime.now().time())
