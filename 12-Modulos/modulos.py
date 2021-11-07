bbb100c13b9ced74f5764f43bf993a1dfc47ec08
jdrosas <juandiazjdrd@gmail.com>
2021-11-06 22:23:45 -0500

    Modulos

--- 12-Modulos/modulos.py
+++ 12-Modulos/modulos.py
@@ -6,3 +6,14 @@ https://docs.python.org/3/py-modindex.html
 Podemos conseguir modulos que ya vienen en el lenguaje,
 modulos en internet y tambien podemos crear nuestros modulos
 """
+
+# Importar modulo propio
+# import mimodulo
+# from mimodulo import holaMundo
+from mimodulo import *
+
+#print(mimodulo.holaMundo("Juan David"))
+#print(mimodulo.calculadora(3, 5, True))
+
+print(holaMundo("Juan David"))
+print(calculadora(5, 4, True))
