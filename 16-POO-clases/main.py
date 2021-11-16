6854983134ac615038a4913c2f4217ccbe3f5e66
jdrosas <juandiazjdrd@gmail.com>
2021-11-15 19:36:00 -0500

    OOP - Multiples objetos

--- 16-POO-clases/main.py
+++ 16-POO-clases/main.py
@@ -3,7 +3,7 @@
 # Definir una clase (nolde para crear mas objetos de ese tipo
 # (coche) cib caracacteristicas similares) 
 
-class coche:
+class Coche:
 
     # Atributos o propiedades (variables)
     # Caracteristicas del coche
@@ -39,13 +39,15 @@ class coche:
 # fin definicio clase
 
 # Crear objeto / Instaciar la clase
-coche = coche()
+coche = Coche()
 
-coche.setColor("amarillo")
+print("----------------COCHE 1--------------------")
+
+coche.setColor("Amarillo")
 coche.setModelo("Murcielago")
 
-print(coche.marca, coche.getModelo(), coche.getColor)
-print("Velocidad actual: ", coche.getVelocidad)
+print(coche.marca, coche.getModelo(), coche.getColor())
+print("Velocidad actual: ", coche.getVelocidad())
 
 coche.acelerar()
 coche.acelerar()
@@ -53,4 +55,15 @@ coche.acelerar()
 coche.acelerar()
 coche.frenar()
 
-print("Velocidad nueva: ", coche.getVelocidad)
+print("Velocidad nueva: ", coche.getVelocidad())
+
+print("----------------COCHE 2--------------------")
+# Crear mas objetos
+
+coche2 = Coche()
+coche2.setColor("Verde")
+coche2.setModelo("Gallardo")
+
+print(coche2.marca, coche2.getModelo(), coche2.getColor())
+
+print(type(coche2))
