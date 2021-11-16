8079fa10d8f96933b998572c13513088aa4134a3
jdrosas <juandiazjdrd@gmail.com>
2021-11-15 20:27:01 -0500

    OOP - constructor

---
+++ 17-POO-constructor/coche.py
@@ -0,0 +1,58 @@
+class Coche:
+
+    # Atributos o propiedades (variables)
+    # Caracteristicas del coche
+    color = "Rojo"
+    marca = "Ferrari"
+    modelo = "Aventador"
+    velocidad = 300
+    caballaje = 500
+    plazas = 2
+
+    def __init__(self, color, marca, modelo, velocidad, caballaje, plazas):
+        self.color = color
+        self.marca = marca
+        self.modelo = modelo
+        self.velocidad =  velocidad
+        self.caballaje = caballaje
+        self.plazas = plazas
+
+    # Metodos, son acciones que hace el objeto (coche) (funciones)
+    def setColor(self, color):
+        self.color = color
+
+    def getColor(self):
+        return self.color
+
+    def setModelo(self, modelo):
+        self.modelo = modelo
+    
+    def getModelo(self):
+        return self.modelo
+
+    def setMarca(self, marca):
+        self.marca = marca
+    
+    def getMarca(self):
+        return self.marca
+
+    def acelerar(self):
+        self.velocidad += 1
+
+    def frenar(self):
+        self.velocidad -= 1
+
+    def getVelocidad(self):
+        return self.velocidad
+
+    def getInfo(self):
+
+        info = "------ Informacion del coche ------"
+        info += "\n Color: " + self.getColor()
+        info += "\n Marca: " + self.getMarca()
+        info += "\n Modelo: " + self.getModelo()
+        info += "\n Velocidad: " + str(self.getVelocidad())
+
+        return info
+
+# fin definicio clase
