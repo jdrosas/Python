8376a4c216b711beeecfdfc553149a874179bc32
jdrosas <juandiazjdrd@gmail.com>
2021-12-03 14:03:17 -0500

    SQLite

--- 19-Bases-datos/sqlite.py
+++ 19-Bases-datos/sqlite.py
@@ -19,5 +19,23 @@ cursor.execute("CREATE TABLE IF NOT EXISTS productos("+
 # Guardar cambios
 conexion.commit()
 
+# Insertar datos
+cursor.execute("INSERT INTO productos VALUES (null, 'Primer producto', 'Descripcion de mi producto', 550)")
+conexion.commit()
+
+# Listar datos
+cursor.execute("SELECT * FROM productos;")
+productos = cursor.fetchall()
+
+for producto in productos:
+    print("Nombre:", producto[1])
+    print("Descripcion:", producto[2])
+    print("Descripcion:", producto[3])
+    print("\n")
+
+cursor.execute("SELECT titulo FROM productos;")
+producto = cursor.fetchone()
+print(producto)
+
 # Cerar conexion
 conexion.close()
