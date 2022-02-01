39fd81bac3e32bde330ac5c90db567d2d165c136
jdrosas <juandiazjdrd@gmail.com>
2022-02-01 18:16:11 -0500

    PROYECTO

---
+++ 20-Proyecto/main.py
@@ -0,0 +1,8 @@
+"""
+Proyecto Python y MySql:
+- Abrir asistente
+- Login o registro
+- Si elegimos registro, creara un usuario en la bbdd
+- Si elegimos login, identifica al usuario y nos preguntará
+- Crear nota, mostrar notas, borrarlas
+"""
