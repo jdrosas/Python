4cb42d166abdb9ad96d357c6c60514230af38c52
jdrosas <juandiazjdrd@gmail.com>
2022-02-01 17:29:11 -0500

    MYSQL

--- 19-Bases-datos/main.py
+++ 19-Bases-datos/main.py
@@ -4,7 +4,7 @@ import mysql.connector
 database = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd=""
+    passwd="",
     database="master_python"
 )
 
@@ -12,10 +12,27 @@ database = mysql.connector.connect(
 # print(database)
 
 cursor = database.cursor()
-
+"""
 cursor.execute("CREATE DATABASE IF NOT EXISTS master_python")
 
 cursor.execute("SHOW DATABASES")
 
 for bd in cursor:
     print(bd)
+"""
+# Crear tablas
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS vehiculos(
+id int(10) auto_increment not null,
+marca varchar(40) not null,
+modelo varchar(40) not null,
+precio float(10,2) not null,
+CONSTRAINT pk_vehiculo PRIMARY KEY(id)
+)
+""")
+
+cursor.execute("SHOW TABLES")
+
+for table in cursor:
+    print(table)
