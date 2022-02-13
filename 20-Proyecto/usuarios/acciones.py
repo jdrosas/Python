ef5cc9ccacc154c25115141834b16abd24a893e8
jdrosas <juandiazjdrd@gmail.com>
2022-02-13 17:44:14 -0500

    Acciones

--- 20-Proyecto/usuarios/acciones.py
+++ 20-Proyecto/usuarios/acciones.py
@@ -2,12 +2,12 @@ class Acciones:
 
     def Registro(self):
         print("\nOk!! Vamos a registrarte en el sistema")
-        nombre = input("¿Cual es tu nombre?: ")
+        nombre = input("\n¿Cual es tu nombre?: ")
         apellido = input("¿Cuales son tus apellidos?: ")
         email = input("¿Introduce tu correo electronico?: ")
         password = input("¿Introduce tu contraseña?: ")
     
     def Login(self):
-        print("Vale!! Identificate en el sistema")
-        email = input("¿Introduce tu correo electronico?: ")
+        print("\nVale!! Identificate en el sistema")
+        email = input("\n¿Introduce tu correo electronico?: ")
         password = input("¿Introduce tu contraseña?: ")
